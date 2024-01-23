a8c694347646691a9b834186a3936c30073269a1
COM <COM@DESKTOP-L2F7LSG>
2024-01-23 14:44:02 +0900

    프로젝트 생성

---
+++ ex01/src/ex01/variable.java
@@ -0,0 +1,10 @@
+package ex01;
+
+public class variable {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String var1 = "A";
+		System.out.println(var1);
+	}
+}
