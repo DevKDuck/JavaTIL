6fc309fe4240c3fba807cb883357b5d873d40419
DevKDuck <loading95@naver.com>
2024-01-29 11:57:15 +0900

    Lotto 함수로 대체한 코드

--- ex01/assignment/week1/Lotto.java
+++ ex01/assignment/week1/Lotto.java
@@ -1,31 +1,39 @@
 package week1;
 
 public class Lotto {
-
-	public static void main(String[] args) {
-		
-		//뽑은 인덱스에 채워 넣는 방식으로 다시 해보기
-		
-		int [] arr = new int[45];
-		
-		
-		//초기화에 대한 책임 -> 함수로 빼야함(단일 책임 원칙)
+	int [] arr = new int[45];
+	int [] result = new int [6];
+	
+	Lotto() {
 		for (int i=0;i<arr.length;i++) {
 			arr[i] = i+1;
 		}
-				
-		int [] result = new int [6];
-		
-		//구슬을 뽑는 책임 -> 함수로 빼야함 (단일 책임 원칙)
+	}
+	
+	void assign() {
 		for(int i=0;i<6;++i) {
 			int idx = (int)(Math.random()*(arr.length - i));
 			result[i] = arr[idx];
 			arr[idx] = arr[arr.length - 1 - i];
 		}
-		
-		for (int i = 0; i<result.length; i++) {
-			System.out.println(result[i]);
+	}
+	
+	void printLotto() {
+		for (int value: result) {
+			System.out.println(value);
 		}
+	}
+
+	public static void main(String[] args) {
+		
+		Lotto lotto = new Lotto();
+		
+		lotto.assign();
+		
+		lotto.printLotto();
+		
+		
+		
 		//false로 이루어진 45개 볼 
 //		boolean [] ball = new boolean[45];
 //
