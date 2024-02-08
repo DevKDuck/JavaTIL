41157f801d871376ce88a3b2e829efe8bdde1912
DevKDuck <loading95@naver.com>
2024-02-08 14:37:59 +0900

    method reflection exception add

--- ex01/src/Reflection/ReflectionExample.java
+++ ex01/src/Reflection/ReflectionExample.java
@@ -3,40 +3,43 @@ package Reflection;
 import java.lang.reflect.*;
 
 public class ReflectionExample {
-   public static void main(String[] args) throws Exception {
-      
-      if (args.length != 4) {
-         System.out.println("사용법 : java  ReflectionExample 클래스 필드명 값 메소드명");
-         System.exit(0);
-      }
-      
-      Class clazz = Class.forName(args[0]);
+	public static void main(String[] args) throws Exception {
+
+		if (args.length != 4) {
+			System.out.println("사용법 : java  ReflectionExample 클래스 필드명 값 메소드명");
+			System.exit(0);
+		}
+
+		Class clazz = Class.forName(args[0]);
 //    Car car = new Car();
-    //기본생성자 함수 정보 
-    Constructor constructor = clazz.getDeclaredConstructor();
-    //new Car(); 와 같은 동작을 함수로 실행 
-    Object obj = constructor.newInstance();
-    System.out.println(obj);
-    
-    Field field = clazz.getDeclaredField(args[1]);
-    if (field != null) {
-       //car.setModel("현대");
-       //car.model = "현대";
-       field.setAccessible(true);
-       field.set(obj, args[2]);
-    }
-    System.out.println(obj);
-    
-    //메서드 리플렉션
-    Method method = clazz.getDeclaredMethod(args[3]);
-    if (method != null) {
-    	method.setAccessible(true);
-    	method.invoke(obj);
-    }
-   
-      
+		// 기본생성자 함수 정보
+		Constructor constructor = clazz.getDeclaredConstructor();
+		// new Car(); 와 같은 동작을 함수로 실행
+		Object obj = constructor.newInstance();
+		System.out.println(obj);
+
+		Field field = clazz.getDeclaredField(args[1]);
+		if (field != null) {
+			// car.setModel("현대");
+			// car.model = "현대";
+			field.setAccessible(true);
+			field.set(obj, args[2]);
+		}
+		System.out.println(obj);
+
+		// 메서드 리플렉션
+		try {
+			Method method = clazz.getDeclaredMethod(args[3]);
+			if (method != null) {
+				method.setAccessible(true);
+				method.invoke(obj);
+			}
+		} catch (NoSuchMethodException e) {
+			System.out.println(args[3] + "메서드가 존재하지 않습니다.");
+		}
+
 //      Person person = new Person();
-      
+
 //      
 //      Class clazz = Car.class;
 //      //Car car = new Car();
@@ -66,14 +69,15 @@ public class ReflectionExample {
 //         printParameters(parameters);
 //         System.out.println(")");
 //      }
-   }
+	}
+
 //
-   private static void printParameters(Class[] parameters) {
-      for (int i = 0; i < parameters.length; i++) {
-         System.out.print(parameters[i].getName());
-         if (i < (parameters.length - 1)) {
-            System.out.print(",");
-         }
-      }
-   }
+	private static void printParameters(Class[] parameters) {
+		for (int i = 0; i < parameters.length; i++) {
+			System.out.print(parameters[i].getName());
+			if (i < (parameters.length - 1)) {
+				System.out.print(",");
+			}
+		}
+	}
 }
